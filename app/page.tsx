b412546a9f427dda226dd1eadef3982c7285355a
Jonathan <emeraldusk@gmail.com>
2023-09-20 21:44:57 -0300

    feat(blog): removed default posts

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,10 @@ export default function Home() {
           <Link href={post.slug}>
             <h2>{post.title}</h2>
           </Link>
+          <div className="">
+            <span>Published by </span>
+            <a target="_blank" href={`https://github.com/${post.user}`}>{post.user}</a>
+          </div>
           {post.description && <p>{post.description}</p>}
         </article>
       ))}
